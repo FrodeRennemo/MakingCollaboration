392fa37ce8b9ec0519f54108c121e10b74b6d049
marcinsikora <sikora.mk@gmail.com>
2016-09-30 16:31:46 +0200

    Comment page and documentation

--- MakingCollaboration/MakingCollaboration/App_Start/RouteConfig.cs
+++ MakingCollaboration/MakingCollaboration/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MakingCollaboration
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "FrontPage", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
