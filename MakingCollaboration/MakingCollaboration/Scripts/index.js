be6fc98db8397b36fb5576abe8ce3c671f423ec8
marcinsikora <sikora.mk@gmail.com>
2016-09-30 16:14:16 +0200

    Front page UI

---
+++ MakingCollaboration/MakingCollaboration/Scripts/index.js
@@ -0,0 +1,46 @@
+﻿
+
+$(document).ready(function () {
+   
+
+    $('#testBtn').click(function () {
+        var cnt = 4;
+        var btn = $(this);
+        btn.button('loading');
+        setTimeout(function () {
+            cnt++;
+            btn.button('reset');
+            btn.text('  ' + cnt);
+        }, 1000);
+    });
+
+    $('#testBtnDown').click(function () {
+        var cnt = 4;
+        var btn = $(this);
+        btn.button('loading');
+        setTimeout(function () {
+            if (cnt > 0) {
+                cnt--;
+            }
+            btn.button('reset');
+            btn.text('  ' + cnt);
+        }, 1000);
+    });
+
+    $(".btn-post-submit")
+        .click(function () {
+
+            var newPost = $(".post-new").clone();
+            newPost.removeClass("post-new");
+            newPost.appendTo(".new-post-placeholder").fadeIn("slow");
+            $(".new-post-content").html($(".input-textarea").val());
+            $(".new-post-content", newPost).removeClass("new-post-content");
+         
+            return false;
+        });
+
+
+    //
+    //
+});
+
